f7293ae46504d31dd7437c3a85b1d8251e8188d5
James Ogilvie <jameso.ogilvie@gmail.com>
2023-01-14 16:13:19 +1100

    Added SQL Server DbContext

--- MCBAWebApp/Program.cs
+++ MCBAWebApp/Program.cs
@@ -1,7 +1,15 @@
+using MCBADataLibrary.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BankDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankContext"), builder => builder.MigrationsAssembly("MCBAWebApp"));
+});
 
 var app = builder.Build();
 
