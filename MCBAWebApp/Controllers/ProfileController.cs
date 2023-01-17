e67a349a4b7f46a2f3d813f7dc71e50c05c40f48
James Ogilvie <jameso.ogilvie@gmail.com>
2023-01-17 20:51:36 +1100

    Started customer profile implementation

---
+++ MCBAWebApp/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using MCBADataLibrary.Data;
+using MCBADataLibrary.Models;
+using MCBAWebApp.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCBAWebApp.Controllers;
+
+[AuthorizeCustomer]
+public class ProfileController : Controller
+{
+    private readonly BankDbContext _context;
+
+    public ProfileController(BankDbContext context) => _context = context;
+
+    private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID))!.Value;
+
+    public async Task<IActionResult> Index()
+    {
+        var customer = await _context.Customers.FindAsync(CustomerID);
+        return View(customer);
+    }
+}
