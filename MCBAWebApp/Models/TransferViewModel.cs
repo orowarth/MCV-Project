7bc21bc5e0e130233e8f7338e594c759216a4f34
James Ogilvie <jameso.ogilvie@gmail.com>
2023-01-17 12:38:39 +1100

    Started transfer implementation

---
+++ MCBAWebApp/Models/TransferViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MCBADataLibrary.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MCBAWebApp.Models;
+
+public class TransferViewModel
+{
+    public Account? Account { get; set; }
+    public int AccountNumber { get; set; }
+
+    [Required]
+    public int DestinationAccount {  get; set; }
+
+    [MaxLength(30)]
+    public string? Comment { get; set; }
+}
