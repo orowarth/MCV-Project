0bb64fdc72ca65781b866d11501ce18b9450dbcb
James Ogilvie <jameso.ogilvie@gmail.com>
2023-01-18 12:26:48 +1100

    Implemented profile view models

---
+++ MCBAWebApp/Models/PasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCBAWebApp.Models;
+
+public class PasswordViewModel
+{
+    [Required]
+    public required string OldPassword { get; set; }
+
+    [Required]
+    public required string NewPassword { get; set; }
+
+    [Required]
+    public required string NewPasswordRepeat { get; set; }
+
+}
---
+++ MCBAWebApp/Models/ProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MCBADataLibrary.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MCBAWebApp.Models;
+
+public class ProfileViewModel
+{
+    [Required, MaxLength(50)]
+    public required string Name { get; set; }
+
+    [StringLength(maximumLength: 11, MinimumLength = 11)]
+    [RegularExpression(@"(\d{3}\s){2}\d{3}", ErrorMessage = "Must be of the format: XXX XXX XXX")]
+    public string? TFN { get; set; }
+
+    [MaxLength(50)]
+    public string? Address { get; set; }
+    [MaxLength(60)]
+    public string? City { get; set; }
+    public State? State { get; set; }
+
+    [StringLength(maximumLength: 4, MinimumLength = 4)]
+    public string? PostCode { get; set; }
+
+    [StringLength(maximumLength: 12, MinimumLength = 12)]
+    [RegularExpression(@"04\d{2}(\s\d{3}){2}", ErrorMessage = "Must be of the format: 04XX XXX XXX")]
+    public string? Mobile { get; set; }
+}
