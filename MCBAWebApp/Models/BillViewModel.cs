66c1868cfe2252b059f3801a079ee88c4c907208
orowarth <s3868494@student.rmit.edu.au>
2023-01-19 14:23:04 +1100

    added model data and validation for bpay model

--- MCBAWebApp/Models/BillViewModel.cs
+++ MCBAWebApp/Models/BillViewModel.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MCBADataLibrary.Enums;
+using System.ComponentModel.DataAnnotations;
+using MCBAWebApp.Validation;
 namespace MCBAWebApp.Models;
 
 public class BillViewModel
 {
-    
+    public IEnumerable<SelectListItem>? Accounts { get; set; }
+    public IEnumerable<SelectListItem>? Payees { get; set; }
+    public int SelectedAccount { get; set; }
+    public int SelectedPayee { get; set; }
+
+    [Required]
+    [PositiveTwoDecimals(ErrorMessage = "Amount must be positive and have no more than two decimal places")]
+    public decimal Amount { get; set; }
+
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy hh:mm}")]
+    [DataType(DataType.DateTime)]
+    public DateTime ScheduleTime { get; set; }
+
+    public BillPeriod Period { get; set; }
 }
