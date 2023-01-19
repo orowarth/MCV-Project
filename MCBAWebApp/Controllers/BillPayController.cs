4f4698b756f4019105752f69cef8d1313725067c
orowarth <s3868494@student.rmit.edu.au>
2023-01-19 12:45:34 +1100

    added controller for bpay

---
+++ MCBAWebApp/Controllers/BillPayController.cs
@@ -0,0 +1,28 @@
+using MCBADataLibrary.Models;
+using MCBADataLibrary.Data;
+using MCBAWebApp.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MCBAWebApp.Controllers;
+
+[AuthorizeCustomer]
+public class BillPayController : Controller
+{
+    private readonly BankDbContext _context;
+    private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID))!.Value;
+
+    public BillPayController(BankDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var bills = await _context.BillPayments
+            .Where(b => b.Account.CustomerID == CustomerID)
+            .ToListAsync();
+
+        return View(bills);
+    }
+}
