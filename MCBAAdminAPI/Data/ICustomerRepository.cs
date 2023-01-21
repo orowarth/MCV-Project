6ea7154d462ca4bd8890628b57b7315ac3021dd4
orowarth <s3868494@student.rmit.edu.au>
2023-01-21 14:21:07 +1100

    added tasks to customer repo

--- MCBAAdminAPI/Data/ICustomerRepository.cs
+++ MCBAAdminAPI/Data/ICustomerRepository.cs
@@ -6,4 +6,8 @@ public interface ICustomerRepository
 {
     public Task<IEnumerable<Customer>> GetAll();
     public Task<Customer?> GetById(int id);
+    public Task UpdateCustomer(Customer customer);
+    public Task BlockCustomer(int id);
+    public Task UnblockCustomer(int id);
+
 }
