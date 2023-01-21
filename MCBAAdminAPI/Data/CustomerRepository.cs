ea0448999ce43bff4ebb550ed1ed5d6f2aa102eb
James Ogilvie <jameso.ogilvie@gmail.com>
2023-01-21 13:44:24 +1100

    Staterted implementing repository pattern for customer data

---
+++ MCBAAdminAPI/Data/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using MCBADataLibrary.Data;
+using MCBADataLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MCBAAdminAPI.Data;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly BankDbContext _context;
+
+    public CustomerRepository(BankDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Customer>> GetAll()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+
+    public async Task<Customer?> GetById(int id)
+    {
+        return await _context.Customers.FindAsync(id);
+    }
+}
---
+++ MCBAAdminAPI/Data/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using MCBADataLibrary.Models;
+
+namespace MCBAAdminAPI.Data;
+
+public interface ICustomerRepository
+{
+    public Task<IEnumerable<Customer>> GetAll();
+    public Task<Customer?> GetById(int id);
+}
