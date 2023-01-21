4378ab71766d173a548269fbaa5e5882c4f9b4ab
orowarth <s3868494@student.rmit.edu.au>
2023-01-21 15:19:16 +1100

    added customer controller to manage customers

--- MCBAAdminAPI/Controllers/CustomerController.cs
+++ MCBAAdminAPI/Controllers/CustomerController.cs
@@ -1,4 +1,6 @@
 using MCBAAdminAPI.Data;
+using MCBAAdminAPI.Communication;
+using MCBADataLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MCBAAdminAPI.Controllers;
@@ -32,5 +34,47 @@ public class CustomerController : ControllerBase
         }
 
         return Ok(customer);
+    }
+
+    [HttpPut("UpdateCustomer")]
+    public async Task<IActionResult> UpdateCustomer(CustomerDto customer)
+    {
+        var retrievedCustomer = await _customerRepository.GetById(customer.CustomerID);
+
+        if (retrievedCustomer is null)
+        {
+            return NotFound();
+        }
+        await _customerRepository.UpdateCustomer(customer);
+        return Ok();
+    }
+
+    [HttpPut("BlockCustomer/{id}")]
+    public async Task<IActionResult> BlockCustomer(int id)
+    {
+        var retrievedCustomer = await _customerRepository.GetById(id);
+
+        if (retrievedCustomer is null)
+        {
+            return NotFound();
+        }
+        
+        await _customerRepository.BlockCustomer(id);
+
+        return Ok();
+    }
+    [HttpPut("UnblockCustomer/{id}")]
+    public async Task<IActionResult> UnblockCustomer(int id)
+    {
+        var retrievedCustomer = await _customerRepository.GetById(id);
+
+        if (retrievedCustomer is null)
+        {
+            return NotFound();
+        }
+        
+        await _customerRepository.UnblockCustomer(id);
+
+        return Ok();
     }
 }
