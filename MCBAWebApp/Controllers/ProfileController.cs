0d29ebfc99902b2739d953c176e89c4194ecf0fa
James Ogilvie <jameso.ogilvie@gmail.com>
2023-01-22 14:13:47 +1100

    Added customer image logic to controllers

--- MCBAWebApp/Controllers/ProfileController.cs
+++ MCBAWebApp/Controllers/ProfileController.cs
@@ -1,7 +1,9 @@
-﻿using MCBADataLibrary.Data;
+﻿using ImageMagick;
+using MCBADataLibrary.Data;
 using MCBADataLibrary.Models;
 using MCBAWebApp.Filters;
 using MCBAWebApp.Models;
+using MCBAWebApp.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SimpleHashing.Net;
@@ -40,15 +42,86 @@ public class ProfileController : Controller
 
     }
 
+    public async Task<IActionResult> ChangeImage()
+    {
+        var customerImage = await _context.CustomerImages.FirstOrDefaultAsync(i => i.CustomerID == CustomerID);
+        return View(new ImageViewModel
+        {
+            CurrentImage = customerImage
+        });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ChangeImage(ImageViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        string? image = null;
+        if (viewModel.Image is not null)
+        {
+            var maxSize = new MagickGeometry(400, 400)
+            {
+                IgnoreAspectRatio = false,
+            };
+
+            using var magickImage = new MagickImage(await viewModel.Image.GetBytes());
+            magickImage.Format = MagickFormat.Jpg;
+            magickImage.Resize(maxSize);
+            image = magickImage.ToBase64();
+        }
+
+        var customerImage = await _context.CustomerImages
+            .FirstOrDefaultAsync(i => i.CustomerID == CustomerID);
+
+        if (customerImage is null)
+        {
+            _context.CustomerImages.Add(new CustomerImage
+            {
+                CustomerID = CustomerID,
+                Image = image
+            });
+        }
+        else
+        {
+            customerImage.Image = image;
+        }
+
+        if (image is not null)
+        {
+            HttpContext.Session.SetString(nameof(Customer.Image), image!);
+        }
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> DeleteImage()
+    {
+        var customerImage = await _context.CustomerImages.FirstOrDefaultAsync(i => i.CustomerID == CustomerID);
+        if (customerImage is not null)
+        {
+            _context.CustomerImages.Remove(customerImage);
+            await _context.SaveChangesAsync();
+            HttpContext.Session.Remove(nameof(Customer.Image));
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
     [HttpPost]
     public async Task<IActionResult> EditProfile(ProfileViewModel profile)
     {
+
         if (!ModelState.IsValid)
         {
             return View(profile);
         }
 
-        var customer = await _context.Customers.FindAsync(CustomerID);
+        var customer = await _context.Customers
+            .FirstAsync(c => c.CustomerID == CustomerID);
         customer!.Name = profile.Name;
         customer.Address = profile.Address;
         customer.City = profile.City;
