a0427b76313e1866934d620e5d6b359d19eb01ad
orowarth <s3868494@student.rmit.edu.au>
2023-01-24 12:43:17 +1100

    added further tests to customer controller

--- MCBAWebApp.Tests/CustomerControllerTests.cs
+++ MCBAWebApp.Tests/CustomerControllerTests.cs
@@ -68,4 +68,95 @@ public class CustomerControllerTests : IDisposable
         var viewModel = Assert.IsType<ViewResult>(result);
         Assert.IsAssignableFrom<DepositViewModel>(viewModel.Model);
     }
+
+    [Fact]
+    public async Task DepositPost_WhenValid_ReturnsViewResult()
+    {
+        // Arrange
+        var customerController = new CustomerController(_context);
+
+        // Act
+        var result = await customerController.Deposit(new DepositViewModel
+        {
+            Amount = 10,
+            AccountNumber = 4100
+        });
+
+        // Assert
+        var viewModel = Assert.IsType<ViewResult>(result);
+        Assert.Equal("ConfirmDeposit", viewModel.ViewName);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0.001)]
+    public async Task DepositPost_WhenInvalid_ReturnsInvalidModelState(decimal amount)
+    {
+        // Arrange
+        var customerController = new CustomerController(_context);
+
+        // Act
+        var result = await customerController.Deposit(new DepositViewModel
+        {
+            Amount = amount,
+            AccountNumber = 4100
+        });
+
+        // Assert
+        var viewModel = Assert.IsType<ViewResult>(result);
+        Assert.NotEqual("ConfirmDeposit", viewModel.ViewName);
+    }
+
+    [Fact]
+    public async Task Withdraw_ReturnsViewResult()
+    {
+        // Arrange
+        var customerController = new CustomerController(_context);
+
+        // Act
+        var result = await customerController.Withdraw(4100);
+
+        // Assert
+        var viewModel = Assert.IsType<ViewResult>(result);
+        Assert.IsAssignableFrom<WithdrawViewModel>(viewModel.Model);
+    }
+
+    [Fact]
+    public async Task Withdraw_WhenValid_ReturnsViewResult()
+    {
+        // Arrange
+        var customerController = new CustomerController(_context);
+
+        // Act
+        var result = await customerController.Withdraw(new WithdrawViewModel
+        {
+            Amount = 10,
+            AccountNumber = 4100
+        });
+
+        // Assert
+        var viewModel = Assert.IsType<ViewResult>(result);
+        Assert.Equal("ConfirmWithdrawal", viewModel.ViewName);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0.001)]
+    [InlineData(10000)]
+    public async Task Withdraw_WhenInvalid_ReturnsViewResult(decimal amount)
+    {
+        // Arrange
+        var customerController = new CustomerController(_context);
+
+        // Act
+        var result = await customerController.Withdraw(new WithdrawViewModel
+        {
+            Amount = amount,
+            AccountNumber = 4100
+        });
+
+        // Assert
+        var viewModel = Assert.IsType<ViewResult>(result);
+        Assert.NotEqual("ConfirmWithdrawal", viewModel.ViewName);
+    }
 }
